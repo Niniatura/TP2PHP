08cea52b7889734ad44bd15d9d891f23f1419a23
Neli <neli.marcos@gmail.com>
2018-07-30 15:20:06 -0300

    base datos vieja

--- BaseDato.class.php
+++ BaseDato.class.php
@@ -1,22 +1,5 @@
- <?php
-
-
-// error class just incase an error occured when trying to connect
-class __errorClass
-{
-    public function __call($meth, $args)
-    {
-        echo $meth . '() failed! Database connection error!';
-    }
-}
+<?php
 class Connectdb{
-    public $host = "localhost";
-    public $user = "root";
-    public $pass = "";
-    public $db = "blog";
-    public $conn; 
-
-    public function __construct(){
     private $host;
     private $user;
     private $pass;
@@ -28,18 +11,7 @@ class Connectdb{
         $this->db = "blog";
         
         $conn = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
-
-    if ($conn->errorCode == 0)
-        {
-            $this->conn = $conn;
-        }
-        else
-        {
-            // This would prevent the regular PHP error, kinda like my simple error reporting method.
-            $this->conn = new __errorClass();
-        }
         return $conn;
     }
-
-
+}
 ?>
